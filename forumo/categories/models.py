b55f7f71db26c4850f7d4c4219f03a5f3285122a
endiliey <endiliey@gmail.com>
2018-03-29 19:04:10 +0800

    build and migrate models

--- forumo/categories/models.py
+++ forumo/categories/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=100)
+
+class Thread(models.Model):
+    subject = models.CharField(max_length=255)
+    last_updated = models.DateTimeField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='threads')
+    starter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='threads')
+
+class Post(models.Model):
+    message = models.TextField(max_length=4000)
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='+')
