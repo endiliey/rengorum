b19699fea207b996d74a22d6b63020f5b2981fbd
endiliey <endiliey@gmail.com>
2018-03-29 17:48:43 +0800

    create categories app

--- forumo/forumo/urls.py
+++ forumo/forumo/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from categories import views
+
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
