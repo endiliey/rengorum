c9e8d04f3bec423b60f32da61213ea525f5d547b
endiliey <endiliey@gmail.com>
2018-03-31 20:09:18 +0800

    rename project to rengorum

--- backend/rengorum/settings.py
+++ backend/rengorum/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'forumo.urls'
+ROOT_URLCONF = 'rengorum.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'forumo.wsgi.application'
+WSGI_APPLICATION = 'rengorum.wsgi.application'
 
 
 # Database
--- backend/rengorum/wsgi.py
+++ backend/rengorum/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "forumo.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rengorum.settings")
 
 application = get_wsgi_application()
