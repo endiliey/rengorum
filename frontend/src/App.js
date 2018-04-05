957fc14565d3158f18d0dc93e1c23c8b355d7dd7
endiliey <endiliey@gmail.com>
2018-04-05 16:24:54 +0800

    add logo component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Avatar from './components/avatar';
 import Loader from './components/loader';
+import Logo from './components/logo';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <Logo />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
---
+++ frontend/src/components/logo/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './styles.css';
+
+const Logo = () => {
+  return (
+    <div className='logoContainer'>
+      <img src={logo} className="logo" alt="logo" />
+      <div className='logoTitle'>Rengorum</div>
+    </div>
+  );
+};
+
+export default Logo;
