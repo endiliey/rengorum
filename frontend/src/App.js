84cb994db70f68641c0961526be2cef4d77a00fb
endiliey <endiliey@gmail.com>
2018-04-05 13:33:40 +0800

    test avatar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Avatar from './components/avatar';
 
 class App extends Component {
   render() {
@@ -11,7 +12,15 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Avatar
+            avatar={'https://avatars3.githubusercontent.com/u/17883920?v=4'}
+          />
+          <Avatar
+            avatar={'https://www.wowkeren.com/images/photo/kang_han_na.jpg'}
+          />
+          <Avatar
+            avatar={'https://y.gtimg.cn/music/photo_new/T001R300x300M000004Bjyj52RTYOj.jpg?max_age=2592000'}
+          />
         </p>
       </div>
     );
