ca69c55a6326114de5312de235772a1dfb1e7aa0
endiliey <endiliey@gmail.com>
2018-04-07 15:21:35 +0800

    add modal dumb component

---
+++ frontend/src/components/modal/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+export default class Modal extends Component {
+  listenKeyboard = (event) => {
+    if (event.key === 'Escape' || event.keyCode === 27) {
+      this.props.onClose();
+    }
+  };
+
+  componentDidMount () {
+    if (this.props.onClose) {
+      window.addEventListener('keydown', this.listenKeyboard, true);
+    }
+  }
+
+  componentWillUnmount () {
+    if (this.props.onClose) {
+      window.removeEventListener('keydown', this.listenKeyboard, true);
+    }
+  }
+
+  onOverlayClick = () => {
+    this.props.onClose();
+  };
+
+  onDialogClick = (event) => {
+    event.stopPropagation();
+  };
+
+  render () {
+    const overlayStyle = this.props.overlayStyle ? this.props.overlayStyle : {};
+    const contentStyle = this.props.contentStyle ? this.props.contentStyle : {};
+    const dialogStyle = this.props.dialogStyle ? this.props.dialogStyle : {};
+    return (
+      <div>
+        <div className="modal-overlay-div" style={overlayStyle} />
+        <div className="modal-content-div" style={contentStyle} onClick={this.onOverlayClick.bind(this)}>
+          <div className="modal-dialog-div" style={dialogStyle} onClick={this.onDialogClick}>
+            {this.props.children}
+            asdfghjk
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
