ccc27a2d3878c0cdf7a330f72b20ccd4fd5f9e33
endiliey <endiliey@gmail.com>
2018-04-08 18:44:27 +0800

    implement user profile logic

--- frontend/src/components/usermenu/usernav.js
+++ frontend/src/components/usermenu/usernav.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Avatar from '../avatar';
 import Button from '../button';
 import './styles.css';
 
 class UserNav extends Component {
   render() {
-    const test_username = "kang_han_na";
+    const username = this.props.username || "TestUser";
+    const avatar = this.props.avatar || "http://api.adorable.io/avatar/200/TestUser";
     return (
         <div className="userMenu">
           <Button className="dropProfile" type="button">
             <Avatar
               className="userAvatar"
-              avatar={this.props.avatar}
+              avatar={avatar}
             />
-            <p className="username">{this.props.username || test_username}</p>
+            <p className="username">{username}</p>
           </Button>
           <div className="userMenu-content">
-            <a href="https://www.quora.com/">My profile</a>
+            <Link to={`/user/${username}`}>My profile</Link>
             <Button
               className="btn-logout"
               onClick={this.props.logout}
