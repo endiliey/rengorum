3c95623ec1f09c071782f4cc9a737fc0112d5da6
endiliey <endiliey@gmail.com>
2018-04-08 15:38:37 +0800

    setup redux persist

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
-import store from './store';
+import Loader from './components/loader';
+import store, { persistor } from './store';
 import HeaderContainer from './containers/header';
 import ModalContainer from './containers/modal';
 import NotFoundPage from './components/not-found-page';
@@ -12,18 +14,20 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <div className="container">
-        <div>
-          <HeaderContainer />
-          <Switch>
-            <Route exact path="/" component={App} />
-            <Route component={NotFoundPage} />
-          </Switch>
+    <PersistGate loading={<Loader />} persistor={persistor}>
+      <BrowserRouter>
+        <div className="container">
+          <div>
+            <HeaderContainer />
+            <Switch>
+              <Route exact path="/" component={App} />
+              <Route component={NotFoundPage} />
+            </Switch>
+          </div>
+          <ModalContainer />
         </div>
-        <ModalContainer />
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
   ,document.getElementById('root')
 );
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,12 +2,22 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import rootReducer from './reducers';
 import thunkMiddleware from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web and AsyncStorage for react-native
 
 // Redux dev tool extension
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   composeEnhancers(
     applyMiddleware(
       createLogger(),
@@ -15,4 +25,5 @@ const store = createStore(
     )
   )
 );
+export const persistor = persistStore(store);
 export default store;
