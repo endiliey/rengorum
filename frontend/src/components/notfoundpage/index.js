8ade96b701df5174c669d40287b97103b1f2a02f
endiliey <endiliey@gmail.com>
2018-04-28 01:34:17 +0800

    better 404 page

--- frontend/src/components/notfoundpage/index.js
+++ frontend/src/components/notfoundpage/index.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import { Message } from 'semantic-ui-react';
 import './styles.css';
 
 class NotFoundPage extends Component {
   render() {
     return (
-      <div className="not-found-page">
-        <h1>404 - Page Not Found</h1>
-        <img alt="" src="https://i.imgur.com/yG7L3Lb.gif" />
-        <p>Sorry, but the page you were trying to view does not exist.</p>
+      <div>
+        <div className="not-found-page">
+          <Message>
+            <Message.Content>
+              <Message.Header>404 - Page Not Found</Message.Header>
+              Sorry, but the page you were trying to view does not exist.
+            </Message.Content>
+          </Message>
+        </div>
+        <img
+          className="not-found-page-img"
+          alt="not-found" 
+          src="https://i.imgur.com/yG7L3Lb.gif"
+        />
       </div>
     );
   }
