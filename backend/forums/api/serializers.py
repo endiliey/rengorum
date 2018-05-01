756a0610e055a095560e0222d952657006167533
endiliey <endiliey@gmail.com>
2018-05-01 18:41:29 +0800

    forum api return more information

--- backend/forums/api/serializers.py
+++ backend/forums/api/serializers.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework.validators import UniqueValidator
 from django.utils.timezone import now
+from datetime import datetime
 from django.contrib.humanize.templatetags.humanize import naturaltime
-
 from forums.models import Forum
 from threads.models import Thread
 from posts.models import Post
@@ -32,18 +32,8 @@ class ForumListSerializer(serializers.ModelSerializer):
 
     def get_last_activity(self, obj):
         try:
-            post = Post.objects.filter(thread__forum=obj).order_by('-created_at').first()
-            if post:
-                last_activity = {
-                    'thread_id': post.thread.id,
-                    'thread_name': post.thread.name,
-                    'username': post.creator.username,
-                    'avatar': post.creator.profile.avatar,
-                    'pinned': post.thread.pinned,
-                    'naturaltime': naturaltime(post.created_at),
-                }
-            else:
-                thread = Thread.objects.filter(forum=obj).order_by('-last_activity').first()
+            thread = Thread.objects.filter(forum=obj).order_by('-last_activity').first()
+            if thread:
                 last_activity = {
                     'thread_id': thread.id,
                     'thread_name': thread.name,
@@ -51,6 +41,16 @@ class ForumListSerializer(serializers.ModelSerializer):
                     'avatar': thread.creator.profile.avatar,
                     'pinned': thread.pinned,
                     'naturaltime': naturaltime(thread.created_at)
+                }
+            post = Post.objects.filter(thread__forum=obj).order_by('-created_at').first()
+            if post and post.created_at > thread.created_at:
+                last_activity = {
+                    'thread_id': post.thread.id,
+                    'thread_name': post.thread.name,
+                    'username': post.creator.username,
+                    'avatar': post.creator.profile.avatar,
+                    'pinned': post.thread.pinned,
+                    'naturaltime': naturaltime(post.created_at),
                 }
             return last_activity
         except:
@@ -79,12 +79,7 @@ class ForumUpdateSerializer(serializers.ModelSerializer):
         lookup_field = 'slug'
 
 class ForumDetailSerializer(serializers.ModelSerializer):
-    threads = serializers.HyperlinkedRelatedField(
-        many=True,
-        read_only=True,
-        view_name='thread-detail',
-        lookup_field='pk'
-    )
+    threads = serializers.SerializerMethodField()
     class Meta:
         model = Forum
         fields = (
@@ -95,3 +90,44 @@ class ForumDetailSerializer(serializers.ModelSerializer):
         )
         read_only_fields = ('slug',)
         lookup_field = 'slug'
+
+    def get_threads(self, obj):
+        def get_last_activity(thread):
+            try:
+                post = Post.objects.filter(thread=thread).order_by('-created_at').first()
+                if post:
+                    return {
+                        'avatar': post.creator.profile.avatar,
+                        'naturaltime': naturaltime(post.created_at),
+                        'username': post.creator.username,
+                        'name': post.creator.profile.name
+                    }
+                return {
+                    'avatar': thread.creator.profile.avatar,
+                    'naturaltime': naturaltime(thread.created_at),
+                    'username': thread.creator.username,
+                    'name': thread.creator.profile.name
+                }
+            except:
+                return None
+
+        def get_replies_count(thread):
+            return Post.objects.filter(thread=thread).count()
+
+        def get_detail(thread):
+            return {
+                'id': thread.id,
+                'name': thread.name,
+                'pinned': thread.pinned,
+                'creator': thread.creator.username,
+                'avatar': thread.creator.profile.avatar,
+                'naturaltime': naturaltime(thread.created_at),
+                'replies_count': get_replies_count(thread),
+                'last_activity': get_last_activity(thread)
+            }
+
+        try:
+            threads = Thread.objects.filter(forum=obj).order_by('-pinned', '-last_activity')
+            return map(get_detail, threads)
+        except:
+            return []
