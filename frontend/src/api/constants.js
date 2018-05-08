8c4a8eb948add55f426ef899f1e87e0886b09091
endiliey <endiliey@gmail.com>
2018-05-08 14:58:34 +0800

    better use of .env for prod & dev

--- frontend/src/api/constants.js
+++ frontend/src/api/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.REACT_APP_API_URL;
+export const API_URL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_API_URL : process.env.REACT_APP_DEV_API_URL;
 export const USER_URL = API_URL + "user/";
 export const USER_REGISTER_URL = USER_URL + "register/";
 export const USER_LOGIN_URL = USER_URL + "login/";
